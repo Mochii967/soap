775778bb80ba706ca13ad44e00a35a982c56ff5e
Raul Carrillo <39688592+Mochii967@users.noreply.github.com>
2018-11-12 22:37:15 -0800

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,9 +1,8 @@
 <?php
-$servername = "ec2-54-225-98-131.compute-1.amazonaws.com
-";
-$username = "nywtvlxdfpzbeb";
-$password = "efe4ca95ac031b6693492a14d48884c31832df8b28696f8cc80e5f83bdcf4353";
-$dbname = "db1v02bo1b416s";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "users";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
